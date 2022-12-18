e62fec728e325bf7beeffbb2425db7af26fac365
Sitting-All-Day <114463970+Sitting-All-Day@users.noreply.github.com>
2022-12-19 01:54:47 +0900

    코드 수정
    보기 편하게 정리

--- datafile.py
+++ datafile.py
@@ -1,44 +1,52 @@
 import pygame, os, random
 
-DIR_PATH = os.path.dirname(__file__)
+DIR_PATH = os.path.dirname(__file__) # 파일 위치
 DIR_IMAGE = os.path.join(DIR_PATH, 'image')
-DIR_SOUND = os.path.join(DIR_PATH, 'sound')
+DIR_FONT = os.path.join(DIR_PATH, 'font')
 
 WINDOW_SIZE = (960, 640)
 TILE_SIZE = 8
 TILE_BACKGROUNDSIZE = (int(WINDOW_SIZE[0] / 7.5), int(WINDOW_SIZE[1] / 20))
 BACKGROUND_COLOR = (27, 25, 25)
+DEAFAULT_FONT_NAME = "a인생책방T.ttf"
 
+# 스프라이트 시트 클래스
 class SpritSheet:
+    def __init__(self, filename, width, height, max_row, max_col, max_index):
+        baseImage = pygame.image.load(os.path.join(DIR_IMAGE, filename)).convert()
 
-    def createAnimationSet(spriteSheet, index_llist, index_max = none):
-        spr = []
+    return spr
 
-        if index_max == None:
-            for index in index_list:
-                spr.append(spriteSheet.spr[index])
-            else:
-                for index in range(index_list, index_max + 1):
-                    spr.append(spriteSheet.spr[index])
-            return spr
+#텍스트 드로우 함수
+def draw_text(screen, text, size, color, x, y):
+    gameFont = pygame.font.Font(os.path.join(DIR_FONT, DEFAULT_FONT_NAME), size)
+    text_surface = gameFont.render(text, False, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (round(x), round(y))
+    screen.blit(text_surface, text_rect)
 
-# 백그라운드 데이터 생성 함수
-def createBackgroundData
-
-# 백그라운드 이미지 생성 함수
-def createBackgroundImage(tileSpr):
+# 배경 이미지 생성함수
+def createBackImage(tileSpr):
     image = pygame.Surface((int(WINDOW_SIZE[0] / 2), int(WINDOW_SIZE[1] / 12)))
-
+    for row in range(16):
+        for col in range(4):
+            star_case = random.randrange(-(col + 2), 3)
+            if star_case >= 0:
+                image.blit(tileSpr.spr[random.randrange(0, 31)]
+                           , (row * TILE_SIZE * 2 + random.randrange(-4, 5)
+                            , col * TILE_SIZE * 2 + random.randrange(-4, 5)))
+    image.blit(tileSpr.spr[31], (24 * TILE_SIZE, 2 * TILE_SIZE))
+    image.blit(tileSpr.spr[32], (25 * TILE_SIZE, 2 * TILE_SIZE))
+    image.blit(tileSpr.spr[33], (24 * TILE_SIZE, 3 * TILE_SIZE))
+    image.blit(tileSpr.spr[34], (25 * TILE_SIZE, 3 * TILE_SIZE))
     image.set_colorkey((0, 0, 0))
-
     return image
 
 # 애니메이션 행동 변경 함수
-def change_playerAction (frame, action_var, new_var, frameSpd, new_frameSpd, aniMode, new_aniMode):
+def change_playerAction(frame, action_var, new_var, frameSpd, new_frameSpd, aniMode, new_aniMode):
     if action_var != new_var:
-        action_var - new_var
+        action_var = new_var
         frame = 0
         frameSpd = new_frameSpd
         aniMode = new_aniMode
-
     return frame, action_var, frameSpd, aniMode
--- game.py
+++ game.py
@@ -1,70 +1,87 @@
 import pygame, sys
 from datafile import *
 from pygame.locals import *
-pygame.init()
+import pygame.mixer
 
-pygame.display.set_caption('거북목 방지 게임') #화면 타이틀
-clock = pygame.time.Clock()
 
-pygame.init()
+class Game :
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
 
-screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+        pygame.display.set_caption('거북목 방지 게임') #화면 타이틀
+        self.clock = pygame.time.Clock()
 
-spriteSheet_player = SpriteSheet('spriteSheet1.png', 16, 16, 8, 8, 12) #스프라이트 시트에서 이미지 로드
-spriteSheet_enemy = SpriteSheet('spriteSheet2.png', 8, 8, 16, 16, 37)
-spriteSheet_background = SpritSheet('spriteSheet3.png', 8, 8, 16, 16,87)
+        self.screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
-spr_player = {}
-spr_player['stay'] = createAnimationSet(spriteSheet_player, [0])
-spr_player['attacked'] = createAnimationSet(spriteSheet_player, [9, 10, 11])
+        self.gameScore = 100 #초기 점수
 
-spr_enemy = {}
-spr_enemy['stay'] = createAnimationSet(spriteSheet_player, [0])
-spr_enemy['attack'] = createAnimationSet(spriteSheet_player, [9, 10, 11])
+        # 스프라이트 시트에서 이미지 로드
+        self.spriteSheet_player = SpriteSheet('spriteSheet1.png', 16, 16, 8, 8, 12)
+        self.spriteSheet_background = SpritSheet('spriteSheet2.png', 8, 8, 16, 16,87)
 
-createBackgroundData()
-backgroundImage = createBackgroundImage(spriteSheet_background)
+        self.spr_player = {}
+        self.spr_player['stay'] = createSpriteSet(self.spriteSheet_player, [0])
+        self.spr_player['addScore'] = createSpriteSet(self.spriteSheet_player, [9, 10, 11])
+        self.spr_player['subScore'] = createSpriteSet(self.spriteSheet_player, [9, 10, 11])
 
-player_rect = pygame.Rect((TILE_MAPiSIZE[0] * 4, TILE_MAPSIZE[1] * 4 - 14), (6, 14)) # 플레이어 화면에서 고정
-player_movement = [0, 0]
-player_vspeed = 0
-player_flytime = 0 # 공중에 뜬 시간
+        self.background = createBackground(self.spriteSheet_background)
 
-player_action = 'stay'
-player_frame = 0
-player_framespeed = 1
-player_frametimer = 0
-player_animationMode = True #애니메이션 모드 (한번만)
+        self.player_rect = pygame.Rect((TILE_MAPiSIZE[0] * 4, TILE_MAPSIZE[1] * 4 - 14), (6, 14)) # 플레이어 화면에서 고정
+        self.player_movement = [0, 0]
+        self.player_vspeed = 0 # y 가속도
+        self.player_flytime = 0 # 공중에 뜬 시간
 
-# 게임 구동
-while True:
-    screen.fill(BACKGROUND_COLOR)
-    screen.blit(background, (0, 0))
+        self.player_action = 'stay'
+        self.player_frame = 0
+        self.player_frameSpeed = 1
+        self.player_frameTimer = 0
+        self.player_animationMode = True #애니메이션 모드 (한번만)
 
-    screen.blit(spriteSheet_player.spr[0], (320, 240))
-    screen.blit(spriteSheet_enemy_.spr[0], (320, 240))
+        # 게임 구동
+        self.run()
 
-# 플레이어
-    player_movement = [0, 0]
-    player_movement[1] += player_vspeed
+    def run(self):
+        while True:
+            self.screen.fill(BACKGROUND_COLOR) #화면 초기화
 
-    player_vspeed += 0.2
-    if player_vspeed > 3;
-        player_vspeed = 3
+            self.screen.blit(self.background, (0, 0))
 
-# 적
-    enemy_movement = [0, 0]
-    enemy_movement[1] += player_vspeed
+            self.screen.blit(spriteSheet_player.spr[0], (320, 240))
+            self.screen.blit(spriteSheet_enemy_.spr[0], (320, 240))
 
-    enemy_vspeed += 0.2
-    if enemy_vspeed > 3;
-        enemy_vspeed = 3
+        # 플레이어
+            self.player_movement = [0, 0]
+            self.player_movement[1] += player_vspeed
 
-# 이벤트
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+            self.player_vspeed += 0.2
+            if self.player_vspeed > 3:
+                self.player_vspeed = 3
 
-    pygame.display.update()
-    clock.tick(60) #60프레임 제한
+            draw_text(self.screen, "SCORE: " + str(self.gameScore), 8, (238, 238, 230), 200, 140)
+
+        # 적
+            self.enemy_movement = [0, 0]
+            self.enemy_movement[1] += player_vspeed
+
+            self.enemy_vspeed += 0.2
+            if self.enemy_vspeed > 3:
+                self.enemy_vspeed = 3
+
+        # 이벤트
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                #if event.type ==  # 안좋은 자세
+                #    self.gameScore -= 10
+                #if event.type == # 좋은 자세
+                #   self.gameScore += 5
+
+            surf = pygame.transfom.scale(self.screen_scaled, WINDOW_SIZE) # 윈도우 창모드
+            self.screen.blit(surf, (0, 0))
+
+            pygame.display.update()
+            self.clock.tick(60) #60프레임 제한
+
+game = Game() #게임 실행
